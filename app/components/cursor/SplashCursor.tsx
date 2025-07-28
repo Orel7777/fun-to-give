5c483621fecc003988ad45ace092b8e093f5b922
Orel bukris <orelbukris7777@gmail.com>
2025-07-28 12:47:55 +0300

    til

--- app/components/cursor/SplashCursor.tsx
+++ app/components/cursor/SplashCursor.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable */
 "use client";
 import React, { useEffect, useRef } from "react";
 
